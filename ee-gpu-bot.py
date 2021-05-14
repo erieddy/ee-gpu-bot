c5290abfbdef44bfb6c6f3810a73f99030baba72
Eric Eddy <maoknus@gmail.com>
2021-05-14 00:22:04 -0400

    message

---
+++ ee-gpu-bot.py
@@ -0,0 +1,18 @@
+import time
+import re
+import logging
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import sys, getopt
+
+class EEbot():
+    def __init__(self):
+        pass
+
+    def best_buy_bot(self, bb_url, bb_user, bb_pass):
+        pass
+
+    def amazon_bot(self, amzn_url, amzn_user, amzn_pass):
+        pass
