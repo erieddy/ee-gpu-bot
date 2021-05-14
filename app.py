a810ab2191948b97e9f0d08a0feba9507e557bb4
Eric Eddy <maoknus@gmail.com>
2021-05-14 08:08:48 -0400

    adding example config ini

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
 import eebot
+import csv
+
+## start
+load_params(self)
 print('starting app up some more. ')
+
+
+def load_creds():
+    pass
+
+def load_urls():
+    pass
+
+
+def load_params():
+    pass
