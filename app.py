fc3ca6269f35e39dede3bc0073b7783d13a90fa8
Eric Eddy <maoknus@gmail.com>
2021-06-01 23:46:34 -0400

    got the basic driver working

--- app.py
+++ app.py
@@ -1,36 +1,15 @@
-import eebot
+from eebot import EEbot
 import csv
 import logging
-from configparser import ConfigParser
 
-
-def load_creds():
-    pass
-
-def load_urls():
-    pass
-
-
-def load_params():
-    # instantiate
-    config = ConfigParser()
-
-    # parse existing file
-    config.read('config.ini')
-    
-    #Load driver
-    chrome_driver = config.get('global', 'chrome_driver')
-
-    #load creds
-    amazon_user = config.get('amazon', 'amazon_user')
-    amazon_pass = config.get('amazon', 'amazon_pass')
-
-    bestbuy_user = config.get('bestbuy', 'bestbuy_user')
-    bestbuy_pass = config.get('bestbuy', 'bestbuy_pass')
-
-    
 
 
 ## start
-load_params()
-print('starting app')
+logger = logging.getLogger()
+logging.basicConfig(
+    format='[%(asctime)s] %(levelname)-8s  %(name)-12s  %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+logger.info('Starting the EEbot app loader script')
+
+EEbot(logger)
--- eebot.py
+++ eebot.py
@@ -6,10 +6,33 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import sys, getopt
+from configparser import ConfigParser
+
 
 class EEbot():
-    def __init__(self):
-        print('ee bot init')
+    def __init__(self, logger):
+        self.logger = logger
+        logger.info('EE bot is intializing and loading the required paramters')
+
+        # instantiate
+        config = ConfigParser()
+
+        # parse existing file
+        config.read('config.ini')
+        
+        #Load driver
+        self.chrome_driver = config.get('global', 'chrome_driver')
+
+        #load creds
+        self.amazon_user = config.get('amazon', 'amazon_user')
+        self.amazon_pass = config.get('amazon', 'amazon_pass')
+
+        self.bestbuy_user = config.get('bestbuy', 'bestbuy_user')
+        self.bestbuy_pass = config.get('bestbuy', 'bestbuy_pass')
+
+        self.logger.info('Completed loading in the required paramters')
+   
+    def load_urls():
         pass
 
     def best_buy_bot(self, bb_url, bb_user, bb_pass):
