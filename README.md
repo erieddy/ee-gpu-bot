97252cdcb7400692d5a245c8aadad29ed6ab25bc
Eric Eddy <maoknus@gmail.com>
2021-06-01 23:08:17 -0400

    commiting

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # ee-gpu-bot
-here is the readme file
-# second paragraph
-stuff here
+This is my bot to buy GPUs. Use at your own risk. I am not resposible for any unwanted purchases made using this bot. 
+
+# How to use this bot
+To use this bot you will need to update the ini file and execute the python script app.py
+
+Set up steps
+
+1. Install python on your machine
+2. Install the required python modules mentioned in the requirments file. 
+3. Update the config-example.ini file and change the name to config.ini ensure this file in the working directory of the other files. 
+4. Execut the app.py python file and enjoy
--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import eebot
 import csv
-
-## start
-load_params(self)
-print('starting app up some more. ')
+import logging
+from configparser import ConfigParser
 
 
 def load_creds():
@@ -14,4 +12,25 @@ def load_urls():
 
 
 def load_params():
-    pass
+    # instantiate
+    config = ConfigParser()
+
+    # parse existing file
+    config.read('config.ini')
+    
+    #Load driver
+    chrome_driver = config.get('global', 'chrome_driver')
+
+    #load creds
+    amazon_user = config.get('amazon', 'amazon_user')
+    amazon_pass = config.get('amazon', 'amazon_pass')
+
+    bestbuy_user = config.get('bestbuy', 'bestbuy_user')
+    bestbuy_pass = config.get('bestbuy', 'bestbuy_pass')
+
+    
+
+
+## start
+load_params()
+print('starting app')
