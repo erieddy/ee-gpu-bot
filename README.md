718299c66fddc8b25822118c3059b4cf88f6f1c4
Eric Eddy <maoknus@gmail.com>
2021-06-08 00:15:05 -0400

    Amazon fully working including new feature price protection.

--- README.md
+++ README.md
@@ -9,5 +9,6 @@ Set up steps
 1. Install python on your machine
 2. Install the required python modules mentioned in the requirments file. 
 3. Update the config-example.ini file and change the name to config.ini ensure this file in the working directory of the other files. 
+i. To combat Amazon frequently posting items for absurd scalper prices a price max can be set for all purchases. Input numbers only, ommit cents and the dollar sign. 
 4. Update the urls.csv file to add the sites, descriptions and URLs of the items you want to track. Be sure that all site names are lower case and provided in the example. 
 5. Execut the app.py python file and enjoy
--- eebot.py
+++ eebot.py
@@ -107,12 +107,12 @@ class EEbot():
 
         self.logger.info('Ending BestBuy thread')
 
-    def amazon_bot(self, amzn_url, amzn_user, amzn_pass, desc=None):
+    def amazon_bot(self, amzn_url, amzn_user, amzn_pass, amzn_max_price, desc=None):
         #self.logger.info('Amazon URL is %s', amzn_url)
         #self.logger.info('Amazon user is %s', amzn_user)
         browser = webdriver.Chrome(self.chrome_driver)
 
-        #amazon login
+        '''amazon login'''
         browser.get('https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fref%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&')
 
         #fill in email
@@ -133,9 +133,63 @@ class EEbot():
             signInButton[0].click()
         except:
             self.logger.error('Failed sign in to Amazon. Unable to find the login button.')
-        #load item URL
-        browser.get(amzn_url)
+
         time.sleep(2)
+        self.logger.info('Pulling ASIN from URL.')
+        asinCodeTemp = re.findall("B0[A-Z,0-9]\w+", amzn_url)
+        asinCode = asinCodeTemp[0]
+        asinCode = asinCode.strip()
+        self.logger.info('ASIN code is "' + asinCode +'".')
+        url = 'https://www.amazon.com/gp/aws/cart/add.html?AssociateTag=brobot02-20&AWSAccessKeyId=AKIAIVQ27DOD3FLEYHJA&Quantity.1=1&ASIN.1=' + asinCode
+        self.logger.info('Clicking Amazon link for the "%s" with URL "%s"',  desc, url)
+
+        amzn_check_stock = True
+        while amzn_check_stock:
+            time.sleep(2)
+            #Getting page
+            browser.get(url)
+
+            '''Check price to make sure it is not more than the max'''
+            try: 
+                itemPrice = browser.find_element_by_class_name('price.item-row').text
+            except: 
+                self.logger.error('Unable to find the "%s" price, it must be out of stock. Reload page and checking again.', desc)
+                continue
+
+            '''Striping out extra characters in price'''
+            itemPrice = itemPrice.replace('$','')
+            itemPrice = itemPrice.replace(',','')
+            itemPrice = itemPrice.split('.')
+            #<td nowrap="" class="price.item-row" valign="top">$1,699.99</td>
+            self.logger.info('The price of the "%s" is %s.', desc, itemPrice[0])
+            if itemPrice[0] < amzn_max_price:
+                self.logger.info('The price of %s is lower than the max price of %s for item "%s"', itemPrice[0], amzn_max_price, desc)
+                '''Price is good attempting to add to cart}'''
+                try:
+                    browser.find_element_by_name('add').click()
+                except:
+                    self.logger.error('Failed to click add to cart button')
+
+                self.logger.info('Attempting to move item to checkout for %s', desc)
+                try:
+                    browser.find_element_by_name('proceedToRetailCheckout').click()
+                    time.sleep(2)
+                except:
+                    self.logger.error("Can't find move to cart button")
+
+                #place order
+                try:
+                    self.logger.info('Clicking the checkout button on %s', desc)
+                    browser.find_element_by_name('placeYourOrder1').click()
+
+                except:
+                    self.logger.error('Failed to click place order')
+                '''Price is good so breaking out of while loop'''
+                amzn_check_stock = False
+            else: 
+                self.logger.info('The price of %s is higher than the max price of %s for item "%s". Refreshing page and checking again.', itemPrice[0], amzn_max_price, desc)
+            #time.sleep(2)
+
         self.logger.info('Ending Amazon thread')
 
     def newegg_bot(self, newegg_url, newegg_user, newegg_pass, desc=None):
@@ -157,12 +211,13 @@ class EEbot():
         #load creds
         self.amazon_user = config.get('amazon', 'amazon_user')
         self.amazon_pass = config.get('amazon', 'amazon_pass')
+        self.amazon_max_price = config.get('amazon', 'amazon_max_price')
 
         self.bestbuy_user = config.get('bestbuy', 'bestbuy_user')
         self.bestbuy_pass = config.get('bestbuy', 'bestbuy_pass')
 
         self.newegg_user = config.get('newegg', 'newegg_user')
-        self.newegg_pass = config.get('neweegg', 'newegg_pass')    
+        self.newegg_pass = config.get('newegg', 'newegg_pass')    
 
         self.logger.info('Completed loading in the required paramters, now loading URLs')
 
@@ -172,12 +227,14 @@ class EEbot():
             reader = csv.reader(f)
             next(reader)
             for row in reader:
-                if row[0] == 'bestbuy':
+                if row[0] == 'bestbuy2':
                     x = threading.Thread(target=self.best_buy_bot, args=(row[2], self.bestbuy_user, self.bestbuy_pass, row[1],))
                 elif row[0] == 'amazon':
-                    x = threading.Thread(target=self.amazon_bot, args=(row[2], self.amazon_user, self.amazon_pass, row[1],))
+                    x = threading.Thread(target=self.amazon_bot, args=(row[2], self.amazon_user, self.amazon_pass, self.amazon_max_price, row[1],))
+                elif row[0] == 'newegg':
+                    x = threading.Thread(target=self.newegg_bot, args=(row[2], self.newegg_user, self.newegg_pass, row[1],))
                 else:
-                    self.logger.error('The site %s is invalid for row %s. Exiting code', row[0], row)
+                    self.logger.error('The site "%s" is invalid for row %s. Exiting code. Check the case and spelling of the site.', row[0], row)
                     exit()
                 threads.append(x)
                 x.start()
